98d5ed0fa8b1cd6098599d773c9d06f088f5e02e
mughalhazy <mughalhazy@gmail.com>
2026-03-19 11:31:56 +0500

    Add dedicated departments page layout

--- ui/app/departments/page.tsx
+++ ui/app/departments/page.tsx
@@ -1,10 +1,10 @@
-import { PeopleStructurePage } from '@/components/employees/people-structure-page'
+import { Departments } from '@/components/surfaces/Departments'
 import { AppShell } from '@/components/shared/app-shell'
 
 export default function DepartmentsPage() {
   return (
     <AppShell currentPath="/departments">
-      <PeopleStructurePage mode="departments" />
+      <Departments />
     </AppShell>
   )
 }
